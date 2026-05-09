d0b65b7440ed2e05b389e732d56ec3734c0ab418
Mohammed Hindi <moh5408415@gmail.com>
2026-05-09 14:22:41 +0300

    Implemented heart animation on button click

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Git Training Project is running...");
+
+const btn = document.getElementById("loveBtn");
+const container = document.getElementById("hearts-container");
+
+btn.addEventListener("click", () => {
+  for (let i = 0; i < 20; i++) {
+    let heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤️";
+
+    // توزيع عشوائي على كامل الشاشة
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    heart.style.top = Math.random() * window.innerHeight + "px";
+
+    // حجم عشوائي
+    let size = Math.random() * 20 + 15;
+    heart.style.fontSize = size + "px";
+
+    container.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 2000);
+  }
+});
